601046d30e4a7b5fa2c6216f95171c9f3a633e63
jensb <jens-github@spamfreemail.de>
2016-01-30 23:15:11 +0100

    Fix RW2 face positions. Consolidate code for face rectangle calculations. Respect iPhoto's file extension case inconsistency.
    Up next: Respect face rectangle position of rotated photos.

--- iphoto2xmp.rb
+++ iphoto2xmp.rb
@@ -61,7 +61,10 @@ File.directory?(outdir) || Dir.mkdir(outdir)
 
 
 # just some eye candy for output
-class String; def bold; "\e[1m#{self}\e[21m" end; end
+class String
+  def bold; "\e[1m#{self}\e[21m" end
+  def red;  "\e[31m#{self}\e[0m" end
+end
 
 
 # Link photo (original or modified version) to destination directory
@@ -95,6 +98,32 @@ def parse_date(intdate)
   return "" unless intdate
   diff = Time.parse('2001-01-01 +0100')
   Time.at(intdate + diff.to_i).to_datetime.rfc3339
+end
+
+
+# Calculate face position depending on crop/rotation status and file type (special treatment for RW2).
+def calc_faces(faces, mwidth, mheight, raw_factor_x=1, raw_factor_y=1, crop_startx=0, crop_starty=0, crop_width=nil, crop_height=nil)
+  crop_width ||= mwidth
+  crop_height ||= mheight
+  res = faces.collect do |face|
+    topleftx = "%.8f" % ((face['topLeftX'] * raw_factor_x * mwidth - crop_startx) / crop_width)    # OK
+    toplefty = "%.8f" % (((1-face['topLeftY']) * raw_factor_y * mheight - (mheight - crop_height - crop_starty)) / crop_height)
+    width    = "%.8f" % ((face['bottomRightX']-face['topLeftX']).abs * raw_factor_x * mwidth / crop_width)
+    height   = "%.8f" % ((face['bottomRightY']-face['topLeftY']).abs * raw_factor_y * mheight / crop_height)
+    centerx  = "%.8f" % (face['topLeftX'].to_f * raw_factor_x + width.to_f/2)
+    centery  = "%.8f" % (1-face['topLeftY'].to_f * raw_factor_y + height.to_f/2)
+    if crop_startx>0 or crop_starty>0 or crop_width != mwidth or crop_height != mheight
+      puts "  FaceCrop: topLeftX/Y=#{face['topLeftX']}/#{face['topLeftY'].to_f}, master_w/h=#{mwidth}/#{mheight}, crop_startx/y=#{crop_startx}/#{mheight-crop_starty}, crop_w/h=#{crop_width}/#{crop_height}"
+    end
+    { 'topleftx' => topleftx, 'toplefty' => toplefty,
+      'centerx'  => centerx,  'centery' => centery, 'width' => width, 'height' => height,
+      'full_name' => face['full_name'] || "Unknown", 'email' => face['email'] }
+  end
+  str = (raw_factor_x != 1)  ?  "FaceRaw " : (crop_startx != 0 ? "FaceCrop" : "FaceOrig")
+  res.each {|f|
+    puts "  #{str}: #{f['topleftx']} / #{f['toplefty']} (#{f['centerx']} / #{f['centery']}) +#{f['width']} +#{f['height']};  #{f['full_name']}\t "
+  }
+  res
 end
 
 
@@ -141,6 +170,7 @@ masterhead, *masters = librarydb.execute2(
         ,v.processedWidth AS processed_width    -- Width of processed (eg. cropped, rotated) image
 
         ,v.overridePlaceId AS place_id          -- modelId of Properties::RKPlace
+        ,v.faceDetectionRotationFromMaster AS face_rotation
    FROM RKVersion v
     LEFT JOIN RKFolder f ON v.projectUuid=f.uuid
     LEFT JOIN RKMaster m ON m.uuid = v.masterUuid
@@ -202,21 +232,25 @@ masters.each do |photo|
   # doesn't work, various info in RKVersion is different (eg. caption)
   # next if $known["#{basedir}/#{origpath}"]
   # Preview can be mp4, mov, jpg, whatever - but not RAW/RW2, it seems.
+  # Preview has jpg or JPG extension. Try both.
   modpath = "Previews/#{File.dirname(photo['imagepath'])}/#{photo['uuid']}/#{File.basename(photo['imagepath']).gsub(/PNG$|JPG$|RW2$/, 'jpg')}"
+  if photo['mediatype'] != 'VIDT' and !File.exist?("#{basedir}/#{modpath}")
+    modpath = modpath.sub(/jpg$/, 'JPG')
+  end
   origxmppath = link_photo(basedir, outdir, photo, origpath, nil)
   next if done_xmp[origxmppath]    # do not overwrite master XMP twice
   # link_photo needs origpath to do size comparison for modified images
   # only perform link_photo for "non-videos" and when a modified image should exist
   # since iPhoto creates "link mp4" files without real video content for "modified" videos (useless)
   if photo['version_number'].to_i > 0 and photo['mediatype'] != 'VIDT'
-    modxmppath  = link_photo(basedir, outdir, photo, modpath, origpath) 
+    modxmppath  = link_photo(basedir, outdir, photo, modpath, origpath)
   end
 
   # FIXME: Fix size of RW2 files (incorrectly set to 3776x2520, but Digikam sees 3792x2538) (Panasonic LX3)
   # TODO: Get real size of RW2 files (dcraw -i -v $FILE | grep "Image Size" | ...) and use that
   if photo["imagepath"] =~ /RW2$/ and photo["master_height"].to_i == 2520
     photo["raw_factor_h"] = 2538.0 / photo["master_height"].to_f   # for converting face positions
-    photo["raw_factor_w"] = 3792.0 / photo["master_width"].to_f
+    photo["raw_factor_w"] = photo["raw_factor_h"] * 2.0 - 1        # don't ask me. It's not "3792.0 / photo["master_width"].to_f".
     #photo["raw_factor_h"] = 1.01
     #photo["raw_factor_w"] = 1.01
     photo["master_height"] = 2538    # incorrect. iPhoto always uses its own internal (wrong) sizes for crop calculations
@@ -231,7 +265,10 @@ masters.each do |photo|
   puts "##{photo['id']}(##{photo['master_id']}): #{photo['caption']}, #{photo['uuid'][0..9]}…/#{photo['master_uuid'][0..9]}…, create: #{@date_master} / edit: #{@date}".bold
   puts "  Desc: #{photodescs[photo['id'].to_i]}"  if photodescs[photo['id'].to_i]
   puts "  Orig: #{photo["master_height"]}x#{photo["master_width"]} (#{photo["raw_factor_h"]}/#{photo["raw_factor_w"]}), #{origpath} (#{File.exist?("#{basedir}/#{origpath}")})"
-  puts "  Mod : #{photo["processed_height"]}x#{photo["processed_width"]}, #{modpath}, (#{File.exist?("#{basedir}/#{modpath}")})" if File.exist?("#{basedir}/#{modpath}")
+  if modxmppath    # modified version *should* exist
+    print "  Mod : #{photo["processed_height"]}x#{photo["processed_width"]}, #{modpath} "
+    puts File.exist?("#{basedir}/#{modpath}") ? "(true)" : "(missing)".red
+  end
 
   #
   # Build up objects with the metadata in using an ERB template. 
@@ -295,26 +332,6 @@ masters.each do |photo|
   # TODO: Save iPhoto/iOS edit operations in XMP structure (digiKam:history?)
  
 
-  # Link: Faces.apdb::RKDetectedFace::masterUuid == Library.apdb::RKMaster::uuid
-  facehead, *faces = facedb.execute2(
-    "SELECT d.modelId             -- primary key
-         ,d.uuid AS detect_uuid   -- primary key
-         ,d.masterUuid            -- --> Library::RKMaster::uuid
-         ,d.faceKey               -- --> RKFaceName::faceKey
-         ,d.topLeftX    ,d.topLeftY    ,d.topRightX    ,d.topRightY     -- *relative* coordinates within *original* image (0..1)
-         ,d.bottomLeftX ,d.bottomLeftY ,d.bottomRightX ,d.bottomRightY  -- *relative* coordinates within *original* image (0..1)
-         ,d.confidence
-         ,d.rejected
-         ,d.ignore
-         ,n.uuid AS name_uuid
-         ,n.name AS name
-         ,n.fullName AS full_name
-         ,n.email AS email
-      FROM RKDetectedFace d
-      LEFT JOIN RKFaceName n ON n.faceKey=d.faceKey
-      WHERE d.masterUuid='#{photo['master_uuid']}'
-      ORDER BY d.modelId");  # LEFT JOIN because we also want unknown faces
-
   # If photo was edited, check if dimensions were changed (crop, rotate, iOS edit)
   # since this would require recalculation of the face rectangle locations.
   # Unfortunately, the crop info is saved in a PropertyList blob within the 'data' column of the DB.
@@ -358,64 +375,43 @@ masters.each do |photo|
     puts ""
   end
 
+
+  #
   # Add faces to BOTH original and edited images.
+  #
   # If edited image is cropped, modify face rectangle positions accordingly.
   # TODO: both Library::RKVersionFaceContent and Faces::RKDetectedFace contain face rectangle data.
   # Which is better?
   xmp_mod = xmp.dup
 
-  @orig_faces = faces.collect do |face|
-    topleftx = "%.8f" % (face['topLeftX'].to_f)
-    toplefty = "%.8f" % (1-face['topLeftY'].to_f)   # iPhoto counts Y dimension from the bottom, thus "1-y"
-    width    = "%.8f" % ((face['bottomRightX'] - face['topLeftX']).abs)
-    height   = "%.8f" % ((face['bottomRightY'] - face['topLeftY']).abs)
-    centerx  = "%.8f" % (face['topLeftX'].to_f + width.to_f/2)
-    centery  = "%.8f" % (1-face['topLeftY'].to_f + height.to_f/2)
-    { 'topleftx' => topleftx, 'toplefty' => toplefty,
-      'centerx'  => centerx,  'centery' => centery, 'width' => width, 'height' => height,
-      'full_name' => face['full_name'] || "Unknown", 'email' => face['email'] }
-  end
-  @orig_faces.each {|f|
-    puts "  FaceOrig: #{f['topleftx']} / #{f['toplefty']} (#{f['centerx']} / #{f['centery']}) +#{f['width']} +#{f['height']};  #{f['full_name']}\t "
-  }
+  # Link: Faces.apdb::RKDetectedFace::masterUuid == Library.apdb::RKMaster::uuid
+  facehead, *faces = facedb.execute2(
+      "SELECT d.modelId             -- primary key
+         ,d.uuid AS detect_uuid   -- primary key
+         ,d.masterUuid            -- --> Library::RKMaster::uuid
+         ,d.faceKey               -- --> RKFaceName::faceKey
+         ,d.topLeftX    ,d.topLeftY    ,d.topRightX    ,d.topRightY     -- *relative* coordinates within *original, non-rotated* image (0..1)
+         ,d.bottomLeftX ,d.bottomLeftY ,d.bottomRightX ,d.bottomRightY  -- *relative* coordinates within *original, non-rotated* image (0..1)
+         ,d.confidence
+         ,d.rejected
+         ,d.ignore
+         ,n.uuid AS name_uuid
+         ,n.name AS name
+         ,n.fullName AS full_name
+         ,n.email AS email
+      FROM RKDetectedFace d
+      LEFT JOIN RKFaceName n ON n.faceKey=d.faceKey
+      WHERE d.masterUuid='#{photo['master_uuid']}'
+      ORDER BY d.modelId");  # LEFT JOIN because we also want unknown faces
 
-  # RW2 files have no definite resolution (res in iPhoto is buggy / wrong) so they need extra treatment.
-  @rw2_faces = faces.collect do |face|
-    topleftx = "%.8f" % (face['topLeftX'].to_f * photo['raw_factor_w'].to_f)
-    toplefty = "%.8f" % ((1-face['topLeftY'].to_f) * photo['raw_factor_h'].to_f)   # iPhoto counts Y dimension from the bottom, thus "1-y"
-    width    = "%.8f" % ((face['bottomRightX'] - face['topLeftX']).abs * photo['raw_factor_w'].to_f)
-    height   = "%.8f" % ((face['bottomRightY'] - face['topLeftY']).abs * photo['raw_factor_h'].to_f)
-    centerx  = "%.8f" % ((face['topLeftX'].to_f * photo['raw_factor_w'].to_f) + width.to_f/2)
-    centery  = "%.8f" % ((1-face['topLeftY'].to_f) * photo['raw_factor_h'].to_f + height.to_f/2)
-    { 'topleftx' => topleftx, 'toplefty' => toplefty,
-      'centerx'  => centerx,  'centery' => centery, 'width' => width, 'height' => height,
-      'full_name' => face['full_name'] || "Unknown", 'email' => face['email'] }
-  end
-  @rw2_faces.each {|f|
-    puts "  Face_RW2: #{f['topleftx']} / #{f['toplefty']} (#{f['centerx']} / #{f['centery']}) +#{f['width']} +#{f['height']};  #{f['full_name']}\t "
-  }
 
-  # Cropped faces need converted rectangle data. We have
-  #  * topleftx/y (0..1) relative face location inside original image (0..1)
-  #  * crop_startx/y     absolute pixels, y from bottom(!)
-  #  * photo[processed_h/w] absolute pixels
-  #
-  @crop_faces = []
-  @crop_faces = faces.collect do |face|
-    topleftx = "%.8f" % ((face['topLeftX'] * photo['master_width'].to_i - crop_startx) / crop_width)    # OK
-    toplefty = "%.8f" % (((1-face['topLeftY']) * photo['master_height'].to_i - (photo['master_height'].to_i-crop_height-crop_starty)) / crop_height)
-    width    = "%.8f" % ((face['bottomRightX']-face['topLeftX']).abs * photo['master_width'].to_i / crop_width)
-    height   = "%.8f" % ((face['bottomRightY']-face['topLeftY']).abs * photo['master_height'].to_i / crop_height)
-    centerx  = "%.8f" % (face['topLeftX'].to_f + width.to_f/2)
-    centery  = "%.8f" % (1-face['topLeftY'].to_f + height.to_f/2)
-    puts "  FaceCrop: topLeftX/Y=#{face['topLeftX']}/#{face['topLeftY'].to_f}, master_w/h=#{photo['master_width']}/#{photo['master_height']}, crop_startx/y=#{crop_startx}/#{photo['master_height'].to_i-crop_starty}, crop_w/h=#{crop_width}/#{crop_height}"
-    { 'topleftx' => topleftx, 'toplefty' => toplefty,
-      'centerx'  => centerx,  'centery' => centery, 'width' => width, 'height' => height,
-      'full_name' => face['full_name'] || "Unknown", 'email' => face['email'] }
-  end if crop_startx > 0
-  @crop_faces.each {|f|
-    puts "  FaceCrop: #{f['topleftx']} / #{f['toplefty']} (#{f['centerx']} / #{f['centery']}) +#{f['width']} +#{f['height']};  #{f['full_name']}\t "
-  }
+  # calc_faces(faces, width, height, raw_factor_x=1, raw_factor_y=1, crop_startx=0, crop_starty=0, crop_width=nil, crop_height=nil)
+  width = photo['master_width'].to_i
+  height = photo['master_height'].to_i
+  @orig_faces = calc_faces(faces, width, height)
+  @rw2_faces  = calc_faces(faces, width, height, photo['raw_factor_w'] || 1, photo['raw_factor_h'] || 1)
+  @crop_faces = calc_faces(faces, width, height, 1, 1, crop_startx, crop_starty, crop_width, crop_height)
+
 
   # TODO: additionally specify modified image as second version of original file in XMP (DerivedFrom?)
   unless(File.exist?(origxmppath))
